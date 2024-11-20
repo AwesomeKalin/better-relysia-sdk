dc11ef781a5ae715e3f97456c090cfe232e36401
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2024-11-20 12:48:39 +0000

    Added asm test

--- tests.test.js
+++ tests.test.js
@@ -3,21 +3,21 @@ const { expect, test } = require('@jest/globals');
 const randomstring = require('randomstring');
 
 test('test authentication with Relysia', async () => {
-    return await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
         expect(data).not.toEqual('Incorrect Password');
     });
 });
 
 test('test non-existant account', async () => {
     try {
-        return await BetterRelysiaSDK.authenticate('thisaccountdefinitelydoesn\'texist', 'no');
+        await BetterRelysiaSDK.authenticate('thisaccountdefinitelydoesn\'texist', 'no');
     } catch (e) {
         expect(e).toEqual('Account doesn\'t exist');
     }
 });
 
 test('getting user profile', async () => {
-    return await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
         data.getUserProfile().then(data => {
             expect(data.userDetails.userId).toEqual('DnkLULM4bGdn4H6z1cv3iiXqRoT2');
         });
@@ -25,7 +25,7 @@ test('getting user profile', async () => {
 });
 
 test('creating wallet', async () => {
-    return await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
         data.createWallet(randomstring.generate(8)).then(data => {
             expect(data.status).toEqual('success');
         });
@@ -33,7 +33,7 @@ test('creating wallet', async () => {
 });
 
 test('get wallet address', async () => {
-    return await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
         data.getAddress().then(data => {
             expect(data.status).toEqual('success');
         });
@@ -41,7 +41,7 @@ test('get wallet address', async () => {
 });
 
 test('get all addressess', async () => {
-    return await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
         data.getAllAddressess().then(data => {
             expect(data.addressess[0]).toEqual('162ERwDNsav1j5XpZkanHbMj4aQAQ6mZ37');
         });
@@ -49,7 +49,7 @@ test('get all addressess', async () => {
 });
 
 test('test leaderboard', async () => {
-    return await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
         data.leaderboard('ea96d3c5f233409ade6e2eb9bba5f5b4a50a9302-SBP90').then(data => {
             expect(data.leaderboard[0].uid).toEqual('cLa5T8NIPFd7Ei4nwKYjDruWjyI2');
         });
@@ -57,7 +57,7 @@ test('test leaderboard', async () => {
 });
 
 test('get wallets', async () => {
-    return await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
         data.wallets().then(data => {
             expect(data.wallets[0].walletTitle).toEqual('QA1');
         });
@@ -65,7 +65,7 @@ test('get wallets', async () => {
 });
 
 test('get mnemonic', async () => {
-    return await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
         data.mnemonic().then(data => {
             expect(data.mnemonic).toEqual('silver salt ticket wash calm album noodle shift sweet pact good resist');
         });
@@ -73,7 +73,7 @@ test('get mnemonic', async () => {
 });
 
 test('get balance', async () => {
-    return await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
         data.balance().then(data => {
             expect(data.coins[0].protocol).toEqual('BSV');
         });
@@ -81,7 +81,7 @@ test('get balance', async () => {
 });
 
 test('get history', async () => {
-    return await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
         data.history().then(data => {
             expect(data.histories.length).not.toEqual(0);
         });
@@ -89,7 +89,7 @@ test('get history', async () => {
 });
 
 test('sweep private key', async () => {
-    return await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
         data.sweep('L3ufPcAVVwNRhbV6PJArwt2U4KyQkfUNUT8WXWoRq6KdL2bGtct7').then(data => {
             expect(data).not.toEqual('Not a valid private key');
         });
@@ -103,8 +103,16 @@ test('get raw tx', async () => {
 });
 
 test('test asm', async () => {
-    // TODO Add a test for asm()
-    return;
+    await BetterRelysiaSDK.authenticate('satoshiasdsa@gmail.com', '4m4z1ngT3ch').then(data => {
+        data.asm('OP_FALSE OP_RETURN aabbcc', 0).then(data => {
+            fetch(`https://api.whatsonchain.com/v1/bsv/main/tx/hash/${data.txid}`).then(data => {
+                data.json().then(data => {
+                    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 1000);
+                    expect(data.vout[0].scriptPubKey.asm).toEqual('0 OP_RETURN 13417386');
+                });
+            });
+        });
+    });
 });
 
 test('token redemption', async () => {
